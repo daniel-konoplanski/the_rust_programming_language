aa0f6c41334cd2c8dd66ad294645775e4e52a2bd
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2026-02-17 17:37:09 +0100

    Added panic examples

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 fn main()
 {
-    let mut map = HashMap::<String, i32>::new();
+    let mut map = HashMap::new();
 
     map.insert(String::from("X"), 21);
     map.insert(String::from("Y"), 26);
@@ -12,8 +12,29 @@ fn main()
         map.insert(weronika, 23);
     }
 
-    for (name, age) in map
+    for (name, age) in &map
     {
         println!("name: {name}, age: {age}");
     }
+
+    let option = map.get("X");
+
+    let Some(age) = option else {
+        println!("Nothing found");
+        return;
+    };
+
+    println!("age: {}", age);
+
+    map.entry(String::from("X")).or_insert(40);
+
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{scores:?}");
 }
