e16dd5deb01cc6d6199716d91ab3a9ef13d56014
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2026-02-21 16:46:47 +0100

    Added more result examples

--- result/src/main.rs
+++ result/src/main.rs
@@ -5,6 +5,8 @@ use std::{
     io::{Read, Write},
 };
 
+use std::io::{self};
+
 fn example1()
 {
     let greeting_file_result = File::open("hello.txt");
@@ -53,8 +55,37 @@ fn example2()
         .expect("hello.txt should be included in this project");
 }
 
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error>
+{
+    let mut file = File::open("hello.txt")?;
+
+    let mut string = String::new();
+
+    file.read_to_string(&mut string)?;
+
+    return Ok(string);
+}
+
 fn main()
 {
-    // example1();
+    example1();
     example2();
+    read_username_from_file();
+    read_username_from_file2();
 }
