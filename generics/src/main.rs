4e262eaa376984b0d30dfab4254b04368484f717
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2026-02-22 17:09:43 +0100

    Added generic method examples

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -17,7 +17,24 @@ struct Point<T>
     y: T,
 }
 
-fn main() {
+impl<T> Point<T>
+{
+    fn x(&self) -> &T
+    {
+        return &self.x;
+    }
+}
+
+impl Point<f64>
+{
+    fn distance_from_origin(&self) -> f32
+    {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main()
+{
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list);
@@ -28,7 +45,10 @@ fn main() {
     let result = largest(&char_list);
     println!("The largest char is {result}");
 
+    let int_point = Point { x: 5, y: 7 };
+    let float_point = Point { x: 5.0, y: 7.0 };
 
-    let int_point = Point {x: 5, y: 7};
-    let float_point = Point {x: 5.0, y: 7.0};
+    println!("x: {}", float_point.x());
+
+    float_point.distance_from_origin();
 }
