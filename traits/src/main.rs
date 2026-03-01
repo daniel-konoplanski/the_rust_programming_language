57a68f209004bbdbfe2c06ccf2c3ee7db1d6ed8d
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2026-03-01 13:28:10 +0100

    Added more trait exmaples

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -35,9 +35,36 @@ fn print_area2<T: Area>(shape: &T)
     println!("{:.2}", shape.area());
 }
 
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
 fn main()
 {
     let rec = Rectangle::new(4.0, 5.0);
     print_area(&rec);
     print_area2(&rec);
+
+    let pair = Pair::<i32>{x: 5, y: 6};
+
+    pair.cmp_display();
 }
