d1b84536b8ef08531edbb737bec53d313dd43e81
daniel-konoplanski <85637752+daniel-konoplanski@users.noreply.github.com>
2026-03-01 15:49:01 +0100

    Added adder example

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -27,7 +27,7 @@ impl<T> Point<T>
 
 impl Point<f64>
 {
-    fn distance_from_origin(&self) -> f32
+    fn distance_from_origin(&self) -> f64
     {
         (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
